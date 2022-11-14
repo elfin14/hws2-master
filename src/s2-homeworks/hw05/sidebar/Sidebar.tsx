ff4679c67309a7b552a7a3068b450dd7b66c6dc1
Dmitrii <elfin85@gmail.com>
2022-11-15 00:03:03 +0100

    homeworks HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -13,7 +13,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
     const sidebarClass = s.sidebar
         + (open ? ' ' + s.open : '')
     return (
-        <>
+        <div className={s.wrap}>
             {/*затемнение справа от открытого меню*/}
             {open && <div className={s.background} onClick={handleClose}/>}
 
@@ -31,7 +31,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ''
+                        } // делает студент
                     >
                         Pre-junior
                     </NavLink>
@@ -39,7 +41,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-link'}
                         to={PATH.JUNIOR}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ''
+                        } // делает студент
                     >
                         Junior
                     </NavLink>
@@ -47,12 +51,14 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-junior-plus-link'}
                         to={PATH.JUNIOR_PLUS}
                         onClick={handleClose}
-                        // className={...} // делает студент
+                        className={({ isActive }) =>
+                            isActive ? s.active : ''
+                        } // делает студент
                     >
                         Junior Plus
                     </NavLink>
                 </nav>
             </aside>
-        </>
+        </div >
     )
 }
