f225b2b113c2a52b58da185ac85edf1433f4f46a
Dmitrii <elfin85@gmail.com>
2022-11-29 02:08:22 +0100

    homeworks HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
-import { saveState } from './localStorage/localStorage'
+import { restoreState, saveState } from './localStorage/localStorage'
 import s2 from '../../s1-main/App.module.css'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s from './HW6.module.css'
@@ -19,11 +19,7 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-        let valueASString = localStorage.getItem('hw6-editable-span-value')
-        if (valueASString) {
-            let newValue = JSON.parse(valueASString)
-            setValue(newValue)
-        }
+        setValue( restoreState('hw6-editable-span-value',''))
     }
 
     return (
@@ -61,4 +57,4 @@ const HW6 = () => {
     )
 }
 
-export default HW6;
+export default HW6
